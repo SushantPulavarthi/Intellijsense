bcd28e85f571c43221b18646ed608590f813aeff
GitHub Action <action@github.com>
2024-10-29 17:37:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/sushantpulavarthi/intellijsense/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sushantpulavarthi/intellijsense/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.sushantpulavarthi.intellijsense.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sushantpulavarthi.intellijsense.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
